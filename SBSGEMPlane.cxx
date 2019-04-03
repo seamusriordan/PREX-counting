362795030061ec7cd2acf4e99169b9b6a6db87bf
Tao Ye <tao.ye@stonybrook.edu>
2019-04-03 15:54:07 -0400

    Added missing declaration

--- SBSGEMPlane.cxx
+++ SBSGEMPlane.cxx
@@ -272,7 +272,8 @@ Int_t   SBSGEMPlane::Decode( const THaEvData& evdata ){
 	    }
 	    // Average the channels with middle 1/3 signals
 	    Double_t cm_noise = 0;
-	    Int_t n_cutoff = 20 ; 
+	    Int_t n_cutoff = 20 ;
+	    Int_t n_cmn = N_APV25_CHAN - 2*n_cutoff;
 	    for(Int_t strip =n_cutoff; strip< N_APV25_CHAN -n_cutoff ;++strip){
 	      if(arrADCSum[strip+1]<arrADCSum[strip]) // Unless N_CMN_CHAN !=128
 		std::cout << "Sorting went Wrong ! " << std::endl;
