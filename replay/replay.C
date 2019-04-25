ba28ab53457bffd2227359b7d43d82b14fe8d1e3
Seamus Riordan <sriordan@anl.gov>
2019-04-25 15:48:21 -0500

    Put in modified Chandan's base scripts and databases for run 409

--- replay/replay.C
+++ replay/replay.C
@@ -5,18 +5,16 @@
 #include "THaEvent.h"
 #include "THaAnalyzer.h"
 #include "THaApparatus.h"
-
+#pragma cling load("../TreeSearch/libTreeSearch-GEM.so");
+#pragma cling load("../PREX-counting/libprexCounting.so");
 //#include "SBSGEMStand.h"
 //#include "SBSBigBite.h"
 
 void replay(){
+    PREXStand *prex= new PREXStand("prex", "Generic apparatus");
+    MPDGEMTracker *gems = new MPDGEMTracker("gems", "Collection of GEMs in stand");
 
-    gSystem->Load("libsbs.so");
-
-    SBSBigBite   *sbs = new SBSBigBite("sbs", "Generic apparatus");
-    SBSGEMStand *gems = new SBSGEMStand("gems", "Collection of GEMs in stand");
-
-    sbs->AddDetector(gems);
+    prex->AddDetector(gems);
 
   //
   //  Steering script for Hall A analyzer demo
@@ -27,7 +25,7 @@ void replay(){
   // add the two spectrometers with the "standard" configuration
   // (VDC planes, S1, and S2)
   // Collect information about a easily modified random set of channels
-  // (see DB_DIR/*/db_D.dat)
+  // (see DB_DIR/db_D.dat)
   /*
   THaApparatus* DECDAT = new THaDecData("D","Misc. Decoder Data");
   gHaApps->Add( DECDAT );
@@ -41,7 +39,7 @@ void replay(){
   // and executes the output routines.
   THaAnalyzer* analyzer = new THaAnalyzer;
   
-  gHaApps->Add(sbs);
+  gHaApps->Add(prex);
 
   // A simple event class to be output to the resulting tree.
   // Creating your own descendant of THaEvent is one way of
@@ -51,17 +49,22 @@ void replay(){
   // Define the run(s) that we want to analyze.
   // We just set up one, but this could be many.
 //  THaRun* run = new THaRun( "prod12_4100V_TrigRate25_4.dat" );
-  THaRun* run = new THaRun( "5GEM_sample.dat" );
+  //THaRun* run = new THaRun( "/home/chad/data/test_589.dat" );
+  int argc=0;
+  //std::cout<<"enter file name: "<<std::endl;
+  //cin>>argc;
+  argc = 409;
+  THaRun* run = new THaRun(Form("test_%d.dat",argc));
   run->SetLastEvent(-1);
 
   run->SetDataRequired(0);
   run->SetDate(TDatime());
 
-  analyzer->SetVerbosity(3);
+  analyzer->SetVerbosity(10);
   
   // Define the analysis parameters
   analyzer->SetEvent( event );
-  analyzer->SetOutFile( "Afile.root" );
+  analyzer->SetOutFile(Form("test_%d.root",argc));
   // File to record cuts accounting information
   analyzer->SetSummaryFile("summary_example.log"); // optional
   
